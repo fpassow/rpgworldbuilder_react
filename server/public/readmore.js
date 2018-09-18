a0f6f944bca83d00b90b246dcb958afd57c7119d
fpassow <fpassow@yahoo.com>
2018-09-18 09:30:52 -0400

    Cleanup of readmore.js

--- server/public/readmore.js
+++ server/public/readmore.js
@@ -3,11 +3,19 @@
 /* 
 Provides "read more" and "read less" functionality for a web page.
 
-To use, mark some HTML element with the class "readmore-context".
+The user sees a link which says "...read more" at the end of a section of text. 
+When they click it, they see more content, and a link that says "read less".
+And clicking "read less" hides the extra content again.
+
+
+To use, mark some HTML element which contains all the relevant parts with the 
+class "readmore-context".
 
 Within the readmore-context element, incude tags like:
 <a href="#" class="readmore">... read more</a>
 <a href="#" class="readless"> read less</a>
+
+You can change the text "...read more" and "read less" to be anything you want.
 
 Then mark other elements inside the readmore-context element with the class "readmore".
 These elements will be initially hidden. They will appear when the user clicks the "readmore"
@@ -16,6 +24,8 @@ link and be hidden again when the user clicks the "readlist" link.
 You can mark many kinds of elements, including span, ul, ol, div, p, table, etc.
 However, all elements except span will be revealed using display:block. So if an
 element should be display:inline, wrap it in a span and put the "readmore" class on the span.
+
+NOTE: Do not put your "readmore" link inside of an element which will be hidden.
 
 Example:
 
